d3c77b99a7e43a342e3965051470258442a89beb
bhaskar <bhaskar.ug20@nsut.ac.in>
2023-06-05 13:49:55 +0530

    initial project

--- src/main/java/com/springapiproj/ExternalApiPorject/configure/AsyncConfig.java
+++ src/main/java/com/springapiproj/ExternalApiPorject/configure/AsyncConfig.java
@@ -1,4 +1,4 @@
-package codeonedigest.springbootasyncdemo.config;
+package com.springapiproj.ExternalApiPorject.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
